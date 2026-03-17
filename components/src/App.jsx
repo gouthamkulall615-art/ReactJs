12e40c95b6a86ca8362a9453e4565b22885bf04c
Goutham Kulall <gouthamkulall615@gmail.com>
2026-03-17 17:09:22 +0530

    components learnings in react

---
+++ components/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/card";
+import Nav from "./components/nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/src/components/nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div >
+
+  <div className="navBar">
+    this is a Navbar
+  </div>
+    </div>
+  )
+}
+
+export default Nav
