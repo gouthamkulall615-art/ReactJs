626e95c0ea31c194fc92f0237ed0d314cc23b21e
Goutham Kulall <gouthamkulall615@gmail.com>
2026-03-20 18:33:34 +0530

    learned the different types of functions

---
+++ functions/src/App.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const App = () => {
+  // const btnClicked = () => {
+  //   console.log("button is clicked");
+  // };
+
+  // const pageScrolling=(ele)=>{
+  //   if(ele>0){
+  //     console.log('page scrolling.....')
+  //   }else{
+  //     console.log('ulta scrolling')
+  //   }
+  // }
+  return (
+    <div>
+      <h1>hello goutham</h1>
+      {/* <button onClick={btnClicked}>change user</button>
+      <button onClick={btnClicked}>explore this</button> */}
+
+      {/* <button onClick={function(){
+        console.log('hello guys')
+      }}>click karo bhai</button> */}
+{/* 
+      <input
+        onChange={(ele) => {
+          console.log(ele.target.value)
+        }}
+        type="text"
+        placeholder="Enter Name"
+      /> */}
+
+
+      {/* <div onMouseMove={(ele)=>{
+        console.log(ele.clientX)
+      }} className="box"></div> */}
+
+
+{/* <div onWheel={(ele)=>{
+// console.log(ele.deltaY)
+pageScrolling(ele.deltaY)
+}}>
+     <div className="page1"></div>
+     <div className="page2"></div>
+     <div className="page3"></div>
+     </div> */}
+
+     <button>Click</button>
+
+    </div>
+  );
+};
+
+export default App;
